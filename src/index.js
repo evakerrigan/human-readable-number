6d1c713d88a3c8a094cea796ef4c642498ae9bfc
Eva Kerrigan <evakerrigan@gmail.com>
2022-07-09 02:42:11 +0300

    done2

--- src/index.js
+++ src/index.js
@@ -20,8 +20,6 @@ const unit = (((number%100)%10 < 10) && (number%100 > 20)) ? units[(number%100)%
 
 const unit0 = (number%100 < 10) ? units[(number%100)%10] : '';
 
-//number1 = hundred + ' ' + tens + tens0 + ' ' + unit + unit0;
-
 if (hundred !== '') {numeric = hundred} else {numeric = ''};
 if (tens0 !== '' || tens !== '') {numeric = numeric + ' ' + tens0 + tens};
 if (unit0 !== '' || unit !== '') {numeric = numeric + ' ' + unit0 + unit};
